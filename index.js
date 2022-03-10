5bafd03c326e8249303caca23fedd5c97876e12f
ramon-wang-rw <89501579+ramon-wang-rw@users.noreply.github.com>
2022-03-10 00:31:08 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>WebScraper</title>
+    </head>
+    <body>
+        <form class="url-form" method="post">
+            <div>
+                <label>URL</label>
+                <input type="url" 
+                       name="url" 
+                       pattern="https://.*" 
+                       placeholder="https://example.com">
+            </div>
+            <button type='submit' name='submit'>Submit</button>
+        </form>
+        
+</html>
+
